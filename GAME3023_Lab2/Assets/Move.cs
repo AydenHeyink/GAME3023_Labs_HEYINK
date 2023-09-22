500e962b649c0d63d19939bdad14967f94a437d1
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2023-09-22 10:41:32 -0400

    Lab2 collisions done, just need the lights now

--- GAME3023_Lab2/Assets/Move.cs
+++ GAME3023_Lab2/Assets/Move.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void FixedUpdate()
+    {
         var dir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 
-
-        transform.Translate(dir* 3* Time.deltaTime);
+        body.velocity = dir * 3;
+        //transform.Translate(dir * 3 * Time.deltaTime);
     }
 
 
