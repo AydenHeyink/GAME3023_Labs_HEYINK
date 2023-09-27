23345622db0afcd461737a5071c322c143a5494c
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2023-09-27 08:16:19 -0400

    Animations made and started, need to hook them up

---
+++ GAME3023_Lab3/Assets/Scripts/playerBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBehaviour : MonoBehaviour
+{
+    Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        var dir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        body.velocity = dir * 3;
+        //transform.Translate(dir * 3 * Time.deltaTime);
+    }
+}
